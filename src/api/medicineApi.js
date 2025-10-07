be21514ef3d7a27a6c1c7c4d9471cfccf0a16044
Dat LT <datlt.mdc@gmail.com>
2025-10-07 22:16:02 +0700

    add authen

--- src/api/medicineApi.js
+++ src/api/medicineApi.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
 const baseURL = "https://api-schoolhealth.purintech.id.vn/api/Medicine";
+// https://api-schoolhealth.purintech.id.vn/api/Medicine/GetAllMedicines
 const medicineApi = {
   getMedicines: async () => {
-    const response = await axios.get(baseURL + "/GetAllMedicines");
+    const token = localStorage.getItem('token'); // Lấy token từ localStorage
+
+    const response = await axios.get(baseURL + "/GetAllMedicines", {
+      // Su dung ma dinh danh
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
     return response.data;
   },
   getMedicineById: async (id) => {
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const baseURL = "https://api-schoolhealth.purintech.id.vn/api/User";
+//https://api-schoolhealth.purintech.id.vn/api/User/user
 export const userApi = {
   login: async (loginInfo) => {
     console.log("loginInfo to login: ", loginInfo);
     const response = axios
       .post(baseURL + "/user", loginInfo)
       .then((response) => {
-        console.log('response',response.data.token);
+        localStorage.setItem("token", response.data.token);
+        console.log("response", response.data.token);
       });
   },
 };
